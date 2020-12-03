a50bbc4c9b4db41e7b583979792a1aee2df07a0a
Cameron <cameronscummins@gmail.com>
2020-12-03 16:05:43 -0600

    Cleaned up Some Cod

--- Main.cpp
+++ Main.cpp
@@ -84,108 +84,6 @@ void printGridID(std::vector<int> grid, unsigned int row, unsigned int col) {
 	}
 }
 
-std::vector<std::vector<unsigned int>> getCombos() {
-	return {
-		{0, 1, 2, 3},
-		{0, 1, 3, 2},
-		{0, 2, 1, 3},
-		{0, 2, 3, 1},
-		{0, 3, 2, 1},
-		{0, 3, 1, 2},
-		{1, 2, 3, 0},
-		{1, 2, 0, 3},
-		{1, 3, 2, 0},
-		{1, 3, 0, 2},
-		{1, 0, 3, 2},
-		{1, 0, 2, 3},
-		{2, 1, 0, 3},
-		{2, 1, 3, 0},
-		{2, 3, 1, 0},
-		{2, 3, 0, 1},
-		{2, 0, 3, 1},
-		{2, 0, 1, 3},
-		{3, 2, 0, 1},
-		{3, 2, 1, 0},
-		{3, 1, 2, 0},
-		{3, 1, 0, 2},
-		{3, 0, 1, 2},
-		{3, 0, 2, 1}
-	};
-}
-
-// First algorithm
-int algo1(std::vector<PopulationCell> grid, std::vector<int>& districtID, unsigned int num_rows, unsigned int num_cols) {
-	int num_changes = 0;
-	std::vector<std::vector<unsigned int>> combos = getCombos();
-	std::vector<unsigned int> sequence = combos[std::rand() % combos.size()];
-
-	for (auto& operation : sequence) {
-		switch (operation) {
-		case 0:
-			// l -> r
-			for (unsigned int row = 0; row < num_rows; row++) {
-				for (unsigned int col = 0; col < num_cols; col++) {
-					if (col != (num_cols - 1)) {
-						if (grid[row * num_cols + col].lean() == grid[row * num_cols + col + 1].lean()
-							&& districtID[row * num_cols + col + 1] != districtID[row * num_cols + col]) {
-							districtID[row * num_cols + col + 1] = districtID[row * num_cols + col];
-							num_changes++;
-						}
-					}
-				}
-			}
-			break;
-
-		case 1:
-			// t -> b
-			for (unsigned int col = 0; col < num_cols; col++) {
-				for (unsigned int row = 0; row < num_rows; row++) {
-					if (row != (num_rows - 1)) {
-						if (grid[row * num_cols + col].lean() == grid[(row + 1) * num_cols + col].lean()
-							&& districtID[(row + 1) * num_cols + col] != districtID[row * num_cols + col]) {
-							districtID[(row + 1) * num_cols + col] = districtID[row * num_cols + col];
-							num_changes++;
-						}
-					}
-				}
-			}
-			break;
-
-		case 2:
-			// r -> l
-			for (unsigned int row = 0; row < num_rows; row++) {
-				for (int col = num_cols - 1; col >= 0; col--) {
-					if (col != 0) {
-						if (grid[row * num_cols + col].lean() == grid[row * num_cols + col - 1].lean()
-							&& districtID[row * num_cols + col - 1] != districtID[row * num_cols + col]) {
-							districtID[row * num_cols + col - 1] = districtID[row * num_cols + col];
-							num_changes++;
-						}
-					}
-				}
-			}
-			break;
-
-		case 3:
-			// b -> t
-			for (unsigned int col = 0; col < num_cols; col++) {
-				for (int row = num_rows - 1; row >= 0; row--) {
-					if (row != 0) {
-						if (grid[row * num_cols + col].lean() == grid[(row - 1) * num_cols + col].lean()
-							&& districtID[(row - 1) * num_cols + col] != districtID[row * num_cols + col]) {
-							districtID[(row - 1) * num_cols + col] = districtID[row * num_cols + col];
-							num_changes++;
-						}
-					}
-				}
-			}
-			break;
-		}
-	}
-	
-	return num_changes;
-}
-
 int algo2(std::vector<PopulationCell> grid, std::vector<int>& districtID, unsigned int num_rows, unsigned int num_cols) {
 
 	unsigned int index = 0;
@@ -227,31 +125,12 @@ int main() {
 	std::vector<PopulationCell> grid  = genPopGrid(max_population, num_rows, num_cols, parties);
 	std::vector<int> districtID(num_rows*num_cols, 0);
 
-	/* Creating continuous districts of the same identifier "A, B, C"
-	*  In each iteration, keep track of num of changes 'n'
-	*                     l->r     t->b     r->l     b->t
-	*                     n=2      n=4      n=1      n=0
-	   A A B    1 2 3    1 1 3    1 1 3    1 1 3    1 1 3
-	   A C B -> 4 5 6 -> 4 5 6 -> 1 5 3 -> 1 5 3 -> 1 5 3
-	   C C B    7 8 9    7 7 9    7 5 3    5 5 3    5 5 3
-	*/
-
 	// Make ID's for each PopulationCell
 	int id = 0;
 	for (auto& cell : grid) {
 		districtID[id] = id;
 		id++;
 	}
-
-	/**
-	unsigned int count = 0;
-	while (count < 20) {
-		int num = algo1(grid, districtID, num_rows, num_cols);
-		if(num == 0) count++;
-		else count = 0;
-		std::cout << num << std::endl;
-	}
-	*/
 
 	algo2(grid, districtID, num_rows, num_cols);
 	printGrid(grid, num_rows, num_cols);
@@ -260,8 +139,8 @@ int main() {
 
 	std::ofstream dfile("district_output.txt");
 	if (dfile.is_open()) {
-		for (int i = 0; i < num_rows; i++) {
-			for (int j = 0; j < num_cols; j++) {
+		for (unsigned int i = 0; i < num_rows; i++) {
+			for (unsigned int j = 0; j < num_cols; j++) {
 				unsigned int loc = i * num_cols + j;
 				dfile << districtID[loc];
 				if (j != num_cols - 1) dfile << " ";
@@ -272,8 +151,8 @@ int main() {
 	}
 	std::ofstream pfile("party_output.txt");
 	if (pfile.is_open()) {
-		for (int i = 0; i < num_rows; i++) {
-			for (int j = 0; j < num_cols; j++) {
+		for (unsigned int i = 0; i < num_rows; i++) {
+			for (unsigned int j = 0; j < num_cols; j++) {
 				unsigned int loc = i * num_cols + j;
 				pfile << int(grid[loc].lean()[0]) % parties.size();
 				if(j != num_cols-1) pfile << " ";
@@ -285,74 +164,3 @@ int main() {
 
 	return 0;
 }
-
-
-/* Create a districting
-	Districting districting({
-		createDistrict(VOTER_AFFILIATION_A, 12, VOTER_AFFILIATION_B, 25),
-		createDistrict(VOTER_AFFILIATION_A, 5, VOTER_AFFILIATION_B, 19),
-		createDistrict(VOTER_AFFILIATION_A, 65, VOTER_AFFILIATION_B, 17)
-		});
-
-	std::cout << "During the election in this districting, " << districting.total() << " voters participated" << std::endl;
-	std::cout << "This distrcting leans towards Party " << districting.lean() << " by " << districting.majority() << " districts" << std::endl;
-	std::cout << "But has a popular vote of " << districting.popular() << " in favor of Party " << districting.leanPopular() << std::endl;
-	std::cout << "This means the popular voting margin is " << districting.total() - districting.popular() << " votes" << std::endl;
-
-
-
-
-	// l -> r
-		for (unsigned int row = 0; row < num_rows; row++) {
-			for (unsigned int col = 0; col < num_cols; col++) {
-				if (col != (num_cols - 1)) {
-					if (grid[row * num_cols + col].lean() == grid[row * num_cols + col + 1].lean()
-						&& districtID[row * num_cols + col + 1] != districtID[row * num_cols + col]) {
-						districtID[row * num_cols + col + 1] = districtID[row * num_cols + col];
-						num_changes++;
-					}
-				}
-			}
-		}
-
-		// t -> b
-		for (unsigned int col = 0; col < num_cols; col++) {
-			for (unsigned int row = 0; row < num_rows; row++) {
-				if (row != (num_rows - 1)) {
-					if (grid[row * num_cols + col].lean() == grid[(row + 1) * num_cols + col].lean()
-						&& districtID[(row + 1) * num_cols + col] != districtID[row * num_cols + col]) {
-						districtID[(row + 1) * num_cols + col] = districtID[row * num_cols + col];
-						num_changes++;
-					}
-				}
-			}
-		}
-		// r -> l
-		for (unsigned int row = 0; row < num_rows; row++) {
-			for (int col = num_cols - 1; col >= 0; col--) {
-				if (col != 0) {
-					if (grid[row * num_cols + col].lean() == grid[row * num_cols + col - 1].lean()
-						&& districtID[row * num_cols + col - 1] != districtID[row * num_cols + col]) {
-						districtID[row * num_cols + col - 1] = districtID[row * num_cols + col];
-						num_changes++;
-					}
-				}
-			}
-		}
-		// b -> t
-		for (unsigned int col = 0; col < num_cols; col++) {
-			for (int row = num_rows - 1; row >= 0; row--) {
-				if (row != 0) {
-					if (grid[row * num_cols + col].lean() == grid[(row - 1) * num_cols + col].lean()
-						&& districtID[(row - 1) * num_cols + col] != districtID[row * num_cols + col]) {
-						districtID[(row - 1) * num_cols + col] = districtID[row * num_cols + col];
-						num_changes++;
-					}
-				}
-			}
-		}
-
-
-
-
-	*/
--- PopulationCell.cpp
+++ PopulationCell.cpp
@@ -2,42 +2,42 @@
 #include <iostream>
 
 PopulationCell::PopulationCell(unsigned int size, std::string major_party, std::vector<std::string> all_parties, int& latest_ID) {
-    
-    // Create vector will all parties in it
-    std::vector<std::string> parties = all_parties;
-    parties.push_back(major_party);
+	
+	// Create vector will all parties in it
+	std::vector<std::string> parties = all_parties;
+	parties.push_back(major_party);
 
-    // Add other voters randomly
-    for (int index = 0; voters.size() <= size; index++) {
-        latest_ID++;
+	// Add other voters randomly
+	for (int index = 0; voters.size() <= size; index++) {
+		latest_ID++;
 
-        Voter voter(parties[std::rand() % parties.size()], latest_ID);
+		Voter voter(parties[std::rand() % parties.size()], latest_ID);
 
-        voters.push_back(voter);
-    }
+		voters.push_back(voter);
+	}
 
-    updateStats();
+	updateStats();
 }
 PopulationCell::PopulationCell(std::vector<Voter> initial_voters) {
-    voters = initial_voters;
-    updateStats();
+	voters = initial_voters;
+	updateStats();
 }
 void PopulationCell::updateStats() {
-    // Map of party (first value) to the number of affiliated districts (second value)
-    std::map<std::string, unsigned int> affiliation_counts;
+	// Map of party (first value) to the number of affiliated districts (second value)
+	std::map<std::string, unsigned int> affiliation_counts;
 
-    // Cycle through all voters and count the number of voters in each party
-    for (auto& voter : voters) {
-        affiliation_counts[voter.getAffiliationCode()] += 1;
-    }
+	// Cycle through all voters and count the number of voters in each party
+	for (auto& voter : voters) {
+		affiliation_counts[voter.getAffiliationCode()] += 1;
+	}
 
-    // Initialize stats
-    major_party_code = VOTER_AFFILIATION_UNDECIDED;
-    major_party_count = 0;
-    minor_party_code = VOTER_AFFILIATION_UNDECIDED;
-    minor_party_count = 0;
+	// Initialize stats
+	major_party_code = VOTER_AFFILIATION_UNDECIDED;
+	major_party_count = 0;
+	minor_party_code = VOTER_AFFILIATION_UNDECIDED;
+	minor_party_count = 0;
 
-    // Cycle through parties and compare counts
+	// Cycle through parties and compare counts
 	for (auto& party : affiliation_counts) {
 		// If a party with more voters is found
 		if (major_party_count < party.second) {
@@ -56,33 +56,33 @@ void PopulationCell::updateStats() {
 	}
 }
 std::string PopulationCell::lean() {
-    return major_party_code;
+	return major_party_code;
 }
 bool PopulationCell::addVoter(Voter voter_to_add) {
 
-    // Checks if the new voter is a copy of an already-existing voter
-    for(auto &v: voters) {
-        if(v.getID() == voter_to_add.getID()) {
-            return false;
-        }
-    }
-    
-    // If new voter, add to voter
-    voters.push_back(voter_to_add);
-    
-    // If voter was added - True
-    return true;
+	// Checks if the new voter is a copy of an already-existing voter
+	for(auto &v: voters) {
+		if(v.getID() == voter_to_add.getID()) {
+			return false;
+		}
+	}
+	
+	// If new voter, add to voter
+	voters.push_back(voter_to_add);
+	
+	// If voter was added - True
+	return true;
 }
 bool PopulationCell::removeVoterByID(int id) {
-    unsigned int index = 0;
-    
-    // Checks if the ID exists in the voter group
-    for (auto& v : voters) {
-        if (v.getID() == id) {
-            voters.erase(voters.begin() + index);
-            return true;
-        }
-        index++;
-    }
-    return false;
+	unsigned int index = 0;
+	
+	// Checks if the ID exists in the voter group
+	for (auto& v : voters) {
+		if (v.getID() == id) {
+			voters.erase(voters.begin() + index);
+			return true;
+		}
+		index++;
+	}
+	return false;
 }
